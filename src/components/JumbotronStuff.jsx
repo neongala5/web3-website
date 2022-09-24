75bea7b2782ad1b600e2f1bc50fd92ffe5183b7b
neongala5 <carlosmartz555@gmail.com>
2022-09-24 16:25:19 -0500

    text update

--- src/components/JumbotronStuff.jsx
+++ src/components/JumbotronStuff.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const JumbotronStuff = () => {
   return (
     <div>
-      Make the middle stuff here. Change file names too as needed. Also pick either functional components or class components. Doesnt matter. 
+      Make the navbar/jumbotron stuff here. Change file names too as needed. Also pick either functional components or class components. Doesnt matter. 
     </div>
   );
 }
