3f736bc39d890f7b35e2b698745538d1c0f7e681
ZIZHAOCHEN <reinachaos@gmail.com>
2022-09-29 19:40:52 -0500

    navbar & Jumbotron 1st version

--- src/components/JumbotronStuff.jsx
+++ src/components/JumbotronStuff.jsx
@@ -8,12 +8,37 @@ import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import gradient from './JumbotronStuff/gradient.jpeg';
 import logo from './JumbotronStuff/logo.gif';
+import { useState, useRef } from 'react';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { ImgOverlay } from 'image-overlay-react'
+import 'image-overlay-react/dist/index.css'
+import CardGroup from 'react-bootstrap/CardGroup';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Nav from 'react-bootstrap/Nav';
+import { fontWeight } from '@mui/system';
 
 
 export default function JumbotronStuff() {
   return (
-    <Card className="bg-{gradient} text-white">
-    <Card.Img align="center" width="1200" height="600" src = {gradient} alt = "gradient" />
-    </Card>
-  )
+    <Container className="my-5" >
+    {/* Card: about us */}
+    <Row style={{ marginTop: 0, marginBottom: 0 }}>
+        <Card className="text-bottom" text={'black'} style={{ padding: 0 }}>
+            <Card.Body>
+            <Card.Img variant="top" src={logo} />
+            <Card.ImgOverlay>
+            <span style={{ fontSize: 80, fontWeight: 600}}>METASECURE</span>
+            <Card.Text >
+            <span style={{ fontSize: 20, fontWeight: 400}}>
+            Building a Trustworthy Web3 World
+            METASECURE</span>
+            </Card.Text>
+            <Button variant="primary">Explore span<span>&#8595;</span></Button>
+            </Card.ImgOverlay>
+            </Card.Body>
+        </Card>
+    </Row>
+</Container>
+);
 }
